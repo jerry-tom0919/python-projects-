eec631ce4078f6fbe4243ddd0ad5385301c67028
Sanjana <asanjana1903@gmail.com>
2026-03-09 20:39:24 +0530

    Add files via upload

---
+++ python projects/tables_ generator.py
@@ -0,0 +1,7 @@
+num = int(input("Enter the number of table:  "))
+
+print("\nMultiplication tabele for, num")
+print("----------------------------------")
+
+for i in range (1,21):
+    print (f"{num} x {i} = {num*i}")
